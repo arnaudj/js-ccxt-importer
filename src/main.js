7a7b78c0ee1628b89b17020d05cea4f37dc8b303
Arnaud J <arnaudj@users.noreply.github.com>
2018-02-13 17:58:51 +0000

    Fetch trades history
    Pass uid, api key/secret by argument.

--- src/main.js
+++ src/main.js
@@ -12,32 +12,51 @@ cli
   .option('-i, --import <exchange>', 'Import for exchange')
   .option('-p, --proxy [proxyAddress]', 'Proxy URL')
   .option('-l, --list', 'List supported exchanges')
+  .option('-u, --uid <uid>', 'Credentials: exchange user id')
+  .option('-k, --apikey <api key>', 'Credentials: API key')
+  .option('-s, --apisecret <secret>', 'Credentials: API secret')
   .parse(process.argv);
 
 var ccxt = require('ccxt');
 
 if (cli.list) {
   log(ccxt.exchanges);
-  process.exit();
 }
 else if (cli.import) {
   assert(cli.import !== '');
-  assert(cli.proxy !== '' && cli.proxy.startsWith('http'), `Invalid proxy: '${cli.proxy}'`);
-  runForExchange(cli.import);
-}
-else if (!cli.import) {
-  log("Nothing to do");
-  process.exit();
+
+  try {
+    runForExchange(cli.import);
+  } catch (e) {
+    log('Exception: ', e);
+  }
 }
 
 async function runForExchange(exchangeName) {
   log(`* Exchange: ${exchangeName}`);
   let exchange = new ccxt[exchangeName]();
-  
+
   log(`Loading ${exchangeName}...`);
   exchange.proxy = cli.proxy || undefined;
+  assert(cli.proxy === undefined || cli.proxy.startsWith('http'), `Invalid proxy: '${cli.proxy}'`);
 
   await exchange.loadMarkets();
-  log(`* Loaded ${exchangeName}`);
-  log(`> ${exchange.symbols}!`);
+  log(`* Loaded ${exchangeName}: ${exchange.symbols}`);
+
+  if (!exchange.has.fetchMyTrades) {
+    log('Exchanges lacks fetchMyTrades capability');
+    return;
+  }
+
+  exchange.uid = cli.uid || undefined;
+  exchange.apiKey = cli.apikey || undefined;
+  exchange.secret = cli.apisecret || undefined;
+
+  if ([exchange.uid, exchange.apiKey, exchange.secret].some((e) => e === undefined)) {
+    log('Missing your API uid/key/secret');
+    return;
+  }
+
+  let trades = await exchange.fetchMyTrades();
+  log('Trades: \n' + JSON.stringify(trades));
 }
